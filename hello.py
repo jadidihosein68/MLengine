81f99d723a77aaf3651fb423f43239d518406346
Mohammad hossein jadidi <jadidi.hosein@gmail.com>
2019-04-08 19:55:58 +0800

    add test model with size

--- hello.py
+++ hello.py
@@ -4,22 +4,30 @@ from flask import jsonify
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score 
+
 
 app = Flask(__name__)
 music_data = pd.read_csv('music.csv')
+
+@app.route('/')
+def index():
+    return 'I am alive'
+
 @app.route('/predict/gender/<int:gender>/age/<int:age>')
 def show_predict(gender,age):
     try :
         model = joblib.load('music-recommaner.joblib')
     except :
-        return ("call /updateModel")
+        return ("call /trainModel")
 
     predictions = model.predict([[age,gender]])
     return jsonify(str(predictions))
 
 #http://localhost:5000/predict/gender/1/age/32
 
-@app.route('/updateModel')
+@app.route('/trainModel')
 def update_model():
     X = music_data.drop(columns=['genre']) #clean data
     y = music_data['genre'] #clean data
@@ -29,9 +37,7 @@ def update_model():
     df = pd.read_csv('vgsales.csv')
     return jsonify(str(df.shape))
 
-@app.route('/')
-def index():
-    return 'I am alive'
+
 
 @app.route('/giveChart')
 def give_chart():
@@ -41,6 +47,19 @@ def give_chart():
                      class_names=sorted(y.unique()),
                     label='all', rounded = True , filled = True )
     return 'Export the dot file'
+
+@app.route('/testModel/testSize/<float:testSize>')
+def test_model(testSize):  
+    X = music_data.drop(columns=['genre'])
+    y = music_data['genre']
+    X_train, X_test, y_train, y_test = train_test_split (X,y, test_size = testSize)
+    model =DecisionTreeClassifier()
+    model.fit(X_train,y_train)
+    predictions = model.predict(X_test)
+    score = accuracy_score (y_test, predictions)
+    return jsonify(str(score))
+    #Sample : http://localhost:5000/testModel/testSize/0.2
+
 
     #set FLASK_APP=hello.py
     #flask run
