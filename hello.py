f21b789c45583d50761bcdf8d8ca909866275b4f
Mohammad hossein jadidi <jadidi.hosein@gmail.com>
2019-04-08 19:27:03 +0800

    update imp

--- hello.py
+++ hello.py
@@ -1,51 +1,43 @@
 import pandas as pd
 from flask import Flask
 from flask import jsonify
+from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.externals import joblib
 
 app = Flask(__name__)
-model = joblib.load('music-recommaner.joblib')
-
+music_data = pd.read_csv('music.csv')
 @app.route('/predict/gender/<int:gender>/age/<int:age>')
 def show_predict(gender,age):
+    model = joblib.load('music-recommaner.joblib')
     predictions = model.predict([[age,gender]])
     return jsonify(str(predictions))
 
 #http://localhost:5000/predict/gender/1/age/32
 
 
-
-
-@app.route('/')
-def index():
+@app.route('/updateModel')
+def update_model():
+    X = music_data.drop(columns=['genre']) #clean data
+    y = music_data['genre'] #clean data
+    model =DecisionTreeClassifier()
+    model.fit(X,y) # train model 
+    joblib.dump(model,'music-recommaner.joblib') # save model
     df = pd.read_csv('vgsales.csv')
     return jsonify(str(df.shape))
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/')
+def index():
+    return 'I am alive'
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return 'Subpath %s' % subpath
-
-  
-
-@app.route('/about')
-def about():
-    return 'The about page'
+@app.route('/giveChart')
+def give_chart():
+    model = joblib.load('music-recommaner.joblib')
+    tree.export_graphviz(model, out_file='music-recommaner.dot', 
+                     feature_names=['age', 'gender'], 
+                     class_names=sorted(y.unique()),
+                    label='all', rounded = True , filled = True )
+    return 'Export the dot file'
 
     #set FLASK_APP=hello.py
     #flask run
